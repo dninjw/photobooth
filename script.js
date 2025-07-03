347e3df38863a7a77a9cab672d0694e340c653a4
dninjw <ndnbmkhairil@gmail.com>
2025-07-03 13:51:52 +0800

    Update script.js
    add visual countdown logic

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const snap = document.getElementById('snap');
 const start = document.getElementById('start');
 const download = document.getElementById('download');
 const countdownSound = document.getElementById('countdown-sound');
+const countdownText = document.gentlementById('countdown');
 
 start.onclick = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({ video: true });
@@ -12,16 +13,31 @@ start.onclick = async () => {
 };
 
 snap.onclick = () => {
-  countdownSound.play(); // play countdown sound
-  setTimeout(() => {
-    const context = canvas.getContext('2d');
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    context.drawImage(video, 0, 0);
-    const data = canvas.toDataURL('image/png');
-    photo.src = data;
-    photo.style.display = 'block';
-  }, 3000); // 3 seconds countdown
+  let count = 3;
+  countdownText.style.display = 'block';
+  countdownText.textContent = count;  
+  countdownSound.play(); // play countdown song
+  
+  const countdownInterval = setInterval(() => {
+    count--;
+    if(count > 0) {
+      countdownText.textContent = count;
+    } else {
+      clearInterval(countdownInterval);
+      countdownText.textContent = 'SNAP !';
+      setTimeout(() => {
+        countdownText.style.display = 'none';
+        
+        const context = canvas.getContext('2d');
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        context.drawImage(video, 0, 0);
+        const data = canvas.toDataURL('image/png');
+        photo.src = data;
+        photo.style.display = 'block';
+     }, 500); // 3 seconds countdown
+   }
+  }, 1000);
 };
 
 download.onclick = () => {
@@ -29,4 +45,5 @@ download.onclick = () => {
   link.download = 'photo.png';
   link.href = photo.src;
   link.click();
+};
 };
